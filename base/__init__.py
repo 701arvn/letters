f7fd1f268f78817bc46f4ae8ed90c783cd0d2ef6
Evgeny Tataurov <tatauroff@gmail.com>
2013-01-27 23:54:53 +0600

    added mongo-adapted TestCase

--- base/__init__.py
+++ base/__init__.py
@@ -1,6 +1,29 @@
 from hashlib import md5
 from datetime import datetime
+from django.test import TestCase
+from django.conf import settings
+
 
 def get_uniq_hash(request):
     uniq_hash = md5(str(datetime.now()) + request.user.username).hexdigest()[:7]
     return uniq_hash
+
+
+class MongoTestCase(TestCase):
+    """
+        TestCase class that clear the collection between the tests
+    """
+    mongodb_name = 'test_%s' % settings.MONGO_DATABASE_NAME
+
+    def _pre_setup(self):
+        from mongoengine.connection import connect, disconnect
+        disconnect()
+        connect(self.mongodb_name, port=settings.MONGO_PORT)
+        super(MongoTestCase, self)._pre_setup()
+
+    def _post_teardown(self):
+        from mongoengine.connection import get_connection, disconnect
+        connection = get_connection()
+        connection.drop_database(self.mongodb_name)
+        disconnect()
+        super(MongoTestCase, self)._post_teardown()
